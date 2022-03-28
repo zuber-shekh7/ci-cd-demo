b1cff92d6e5743854cdf33f7d2db1c57e3ed5afd
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-28 14:21:32 +0530

    add cors and dotenv config

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
+import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
 
+dotenv.config();
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello world");
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import app from "./app.js";
 
+const PORT = process.env.PORT || 4000;
+
 app.listen(4000, () => {
   console.log(`App is running on http://localhost:4000`);
 });
