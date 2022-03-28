0bb2cf19ebbfcbddea10f8079634c5df11a474c7
Zuber Shekh <zuber.shekh7@gmail.com>
2022-03-28 14:24:24 +0530

    remove static value of PORT

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log(`App is running on http://localhost:4000`);
 });
